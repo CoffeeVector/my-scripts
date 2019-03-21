963003100848cba1c3d8dec73e39e1b9fd088522
coffeevector <coffeevector@gmail.com>
2019-03-20 23:18:16 -0700

    Notification for when the compilation fails for any reason

--- otflatex/otflatexgen.sh
+++ otflatex/otflatexgen.sh
@@ -14,8 +14,15 @@ echo "$\end{document}" >> ~/scripts/otflatex/cache/eq.tex
 
 pdflatex -output-directory ~/scripts/otflatex/cache/ ~/scripts/otflatex/cache/eq.tex
 
+if [ ! -f ~/scripts/otflatex/cache/eq.pdf ]; then
+	notify-send "LaTeX compile failed!"
+	exit 1
+fi
+
 convert -flatten -density 300 -quality 100% ~/scripts/otflatex/cache/eq.pdf ~/scripts/otflatex/cache/eq.png
 
 xclip -selection clipboard -t image/png -i ~/scripts/otflatex/cache/eq.png
 
+rm ~/scripts/otflatex/cache/eq.pdf
+rm ~/scripts/otflatex/cache/eq.png
 notify-send "LaTeX Compiled!"
