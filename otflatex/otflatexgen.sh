0f7c448d7d91f7ce3295f2863c9e61391d06bd42
coffeevector <coffeevector@gmail.com>
2019-03-20 23:08:25 -0700

    Added message for competion and added padding

--- otflatex/otflatexgen.sh
+++ otflatex/otflatexgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "\documentclass[preview, 12pt]{standalone}
+echo "\documentclass[preview, border={5pt 5pt 5pt 5pt}]{standalone}
 \usepackage{amsmath}
 \usepackage{amssymb}
 \begin{document}
@@ -9,6 +9,8 @@ echo "$\end{document}" >> ~/scripts/otflatex/cache/eq.tex
 
 pdflatex -output-directory ~/scripts/otflatex/cache/ ~/scripts/otflatex/cache/eq.tex
 
-convert -flatten -density 300 -quality 90 ~/scripts/otflatex/cache/eq.pdf ~/scripts/otflatex/cache/eq.png
+convert -flatten -density 300 -quality 100% ~/scripts/otflatex/cache/eq.pdf ~/scripts/otflatex/cache/eq.png
 
 xclip -selection clipboard -t image/png -i ~/scripts/otflatex/cache/eq.png
+
+notify-send "LaTeX Compiled!"
