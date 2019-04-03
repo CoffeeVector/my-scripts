85a4f5d2b7db4077b5355a620ebe73f5f9d5ddbb
coffeevector <coffeevector@gmail.com>
2019-04-02 17:49:11 -0700

    Moved the cursor to the 8th line

--- otflatex/otflatexgen.sh
+++ otflatex/otflatexgen.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-echo "\documentclass[preview, border={5pt 5pt 5pt 5pt}]{standalone}
-\usepackage{amsmath}
-\usepackage{amssymb}
-\usepackage{physics}
-\usepackage{xcolor}
-\begin{document}
-$\displaystyle" > ~/scripts/otflatex/cache/eq.tex
-st -e vim '+set syntax=latex' ~/scripts/otflatex/cache/intermediate.tex
-if [ "$(cat ~/scripts/otflatex/cache/intermediate.tex)" = "" ]; then
-	notify-send "EXITING, NOTHING INPUTTED"
-	exit 1
-fi
-cat ~/scripts/otflatex/cache/intermediate.tex >> ~/scripts/otflatex/cache/eq.tex
-echo "$\end{document}" >> ~/scripts/otflatex/cache/eq.tex
-
-pdflatex -output-directory ~/scripts/otflatex/cache/ ~/scripts/otflatex/cache/eq.tex
-
+st -e vim ~/scripts/otflatex/cache/eq.tex +8
+wait
 if [ ! -f ~/scripts/otflatex/cache/eq.pdf ]; then
 	notify-send "LaTeX compile failed!"
 	xclip -selection clipboard < ~/scripts/otflatex/cache/intermediate.tex
-	rm ~/scripts/otflatex/cache/intermediate.tex
 	exit 1
 fi
 
@@ -27,7 +11,4 @@ convert -flatten -density 300 -quality 100% ~/scripts/otflatex/cache/eq.pdf ~/sc
 
 xclip -selection clipboard -t image/png -i ~/scripts/otflatex/cache/eq.png
 
-rm ~/scripts/otflatex/cache/eq.pdf
-rm ~/scripts/otflatex/cache/eq.png
-rm ~/scripts/otflatex/cache/intermediate.tex
 notify-send "LaTeX Compiled!"
