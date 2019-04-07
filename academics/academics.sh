f656b13f0a63a322bfd37fadec9ea0ff19b694bb
coffeevector <coffeevector@gmail.com>
2019-04-06 17:29:40 -0700

    Removed print statements and also made compatible with new ranger update concerning the use case ranger <filename>. Before, ranger <filename> was able to open the file in the respective application, but now it's preferred that the behavior of ranger --selectfile <filename> opens a new ranger with the file selected. To achieve the old behavior, the command would be rifle <filename>

--- academics/academics.sh
+++ academics/academics.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 len=$(ls -l ~/Academics/Current/| wc -l)
-echo $len
 course=$(ls -l ~/Academics/Current/ | awk '{ if (NR !=1) {print $9}}' | sed "s/\([A-Z]\)/ \1/g" | awk '{if (substr($0, 1, 1) ~ / /){print substr($0, 2)} else {print $0} }' | rofi -dmenu -lines $len -i -p "Academics")
 course=$(echo -e $course | sed "s/ //g" )
 if [ "$course" != "" ]; then
-	st -e ranger ~/Academics/Current/$course &
+	if [[ -f ~/Academics/Current/$course ]]; then
+		st -e rifle ~/Academics/Current/$course &
+	else
+		st -e ranger ~/Academics/Current/$course &
+	fi
 fi
