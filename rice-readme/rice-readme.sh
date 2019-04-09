0eacc7aef2d7da639aa813af9395f65dc1377d3c
coffeevector <coffeevector@gmail.com>
2019-04-09 10:45:58 -0700

    Removed cache/ all necessary files are in /tmp

--- rice-readme/rice-readme.sh
+++ rice-readme/rice-readme.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+if [ ! -d /tmp/rice-readme/ ]; then
+	mkdir /tmp/rice-readme/
+fi
 echo "\documentclass{article}
 \title{Read Me for Kevin Zheng's Rice}
 \author{Kevin Zheng}
 \begin{document}
 \maketitle
-\section{i3 hotkeys}" > ~/scripts/rice-readme/cache/readme.tex
-cat ~/.config/i3/config | grep -v "#bindsym" | grep -e "bindsym" -e "# #" |sed "s/.*bindsym.*/\\\subsection{&}/"| sed "s/\\$/\\\\$/g" | sed "s/_/\\\_/" | sed "s/.*# #.*/&\\\\\\\/" | sed "s/# #//" >> ~/scripts/rice-readme/cache/readme.tex
-echo "\end{document}" >> ~/scripts/rice-readme/cache/readme.tex
-pdflatex --output-directory ~/scripts/rice-readme/cache/ ~/scripts/rice-readme/cache/readme.tex
-evince ~/scripts/rice-readme/cache/readme.pdf &
+\section{i3 hotkeys}" > /tmp/rice-readme/readme.tex
+cat ~/.config/i3/config | grep -v "#bindsym" | grep -e "bindsym" -e "# #" |sed "s/.*bindsym.*/\\\subsection{&}/"| sed "s/\\$/\\\\$/g" | sed "s/_/\\\_/" | sed "s/.*# #.*/&\\\\\\\/" | sed "s/# #//" >> /tmp/rice-readme/readme.tex
+echo "\end{document}" >> /tmp/rice-readme/readme.tex
+pdflatex --output-directory /tmp/rice-readme/ /tmp/rice-readme/readme.tex
+evince /tmp/rice-readme/readme.pdf &
