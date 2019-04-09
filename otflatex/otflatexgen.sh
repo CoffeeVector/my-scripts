abe91adb63bc7f4acb3e2cafb07b0178dfc76623
coffeevector <coffeevector@gmail.com>
2019-04-09 10:42:52 -0700

    Removed cache/, all necessary files are in /tmp

--- otflatex/otflatexgen.sh
+++ otflatex/otflatexgen.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-st -e vim ~/scripts/otflatex/cache/eq.tex +8
-wait
-if [ ! -f ~/scripts/otflatex/cache/eq.pdf ]; then
+if [ ! -d /tmp/otflatex/ ]; then
+	mkdir /tmp/otflatex/
+fi
+if [ ! -f /tmp/otflatex/eq.tex ]; then
+	echo "\documentclass[preview, border={5pt 5pt 5pt 5pt}]{standalone}
+	\usepackage{amsmath}
+	\usepackage{amssymb}
+	\usepackage{amsthm}
+	\usepackage{physics}
+	\usepackage{xcolor}
+	\usepackage{tikz}
+	\begin{document}
+	\end{document}" > /tmp/otflatex/eq.tex
+fi
+st -e vim /tmp/otflatex/eq.tex +8
+# Assume that the tex will be compiled using my .vimrc
+if [ ! -f /tmp/otflatex/eq.pdf ]; then
 	notify-send "LaTeX compile failed!"
-	xclip -selection clipboard < ~/scripts/otflatex/cache/intermediate.tex
 	exit 1
 fi
+convert -flatten -density 300 -quality 100% /tmp/otflatex/eq.pdf /tmp/otflatex/eq.png
+rm /tmp/otflatex/eq.pdf
 
-convert -flatten -density 300 -quality 100% ~/scripts/otflatex/cache/eq.pdf ~/scripts/otflatex/cache/eq.png
-rm ~/scripts/otflatex/cache/eq.pdf
-
-xclip -selection clipboard -t image/png -i ~/scripts/otflatex/cache/eq.png
+xclip -selection clipboard -t image/png -i /tmp/otflatex/eq.png
 
 notify-send "LaTeX Compiled!"
