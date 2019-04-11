ece4cf380ab8acd66f53575958c044ca39c0a125
coffeevector <coffeevector@gmail.com>
2019-04-11 14:32:28 -0700

    Each hotkey is not in an \item instead of a \section

--- rice-readme/rice-readme.sh
+++ rice-readme/rice-readme.sh
@@ -7,8 +7,10 @@ echo "\documentclass{article}
 \author{Kevin Zheng}
 \begin{document}
 \maketitle
-\section{i3 hotkeys}" > /tmp/rice-readme/readme.tex
-cat ~/.config/i3/config | grep -v "#bindsym" | grep -e "bindsym" -e "# #" |sed "s/.*bindsym.*/\\\subsection{&}/"| sed "s/\\$/\\\\$/g" | sed "s/\\~/\\$\\\sim\\$/g" | sed "s/_/\\\_/" | sed "s/.*# #.*/&\\\\\\\/" | sed "s/# #//" >> /tmp/rice-readme/readme.tex
-echo "\end{document}" >> /tmp/rice-readme/readme.tex
+\section{i3 hotkeys}
+\begin{enumerate}" > /tmp/rice-readme/readme.tex
+cat ~/.config/i3/config | grep -v "#bindsym" | grep -e "bindsym" -e "# #" |sed "s/.*bindsym.*/\\\item {\\\bfseries &}\\\\\\\/"| sed "s/\\$/\\\\$/g" | sed "s/\\~/\\$\\\sim\\$/g" | sed "s/_/\\\_/" | sed "s/# #//" >> /tmp/rice-readme/readme.tex
+echo "\end{enumerate}
+\end{document}" >> /tmp/rice-readme/readme.tex
 pdflatex --output-directory /tmp/rice-readme/ /tmp/rice-readme/readme.tex
 evince /tmp/rice-readme/readme.pdf &
