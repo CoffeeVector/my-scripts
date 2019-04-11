957882caee14121e2f57fa5d65d8ccb856d79060
coffeevector <coffeevector@gmail.com>
2019-04-10 17:27:44 -0700

    Fixed tildes

--- rice-readme/rice-readme.sh
+++ rice-readme/rice-readme.sh
@@ -8,7 +8,7 @@ echo "\documentclass{article}
 \begin{document}
 \maketitle
 \section{i3 hotkeys}" > /tmp/rice-readme/readme.tex
-cat ~/.config/i3/config | grep -v "#bindsym" | grep -e "bindsym" -e "# #" |sed "s/.*bindsym.*/\\\subsection{&}/"| sed "s/\\$/\\\\$/g" | sed "s/_/\\\_/" | sed "s/.*# #.*/&\\\\\\\/" | sed "s/# #//" >> /tmp/rice-readme/readme.tex
+cat ~/.config/i3/config | grep -v "#bindsym" | grep -e "bindsym" -e "# #" |sed "s/.*bindsym.*/\\\subsection{&}/"| sed "s/\\$/\\\\$/g" | sed "s/\\~/\\$\\\sim\\$/g" | sed "s/_/\\\_/" | sed "s/.*# #.*/&\\\\\\\/" | sed "s/# #//" >> /tmp/rice-readme/readme.tex
 echo "\end{document}" >> /tmp/rice-readme/readme.tex
 pdflatex --output-directory /tmp/rice-readme/ /tmp/rice-readme/readme.tex
 evince /tmp/rice-readme/readme.pdf &
