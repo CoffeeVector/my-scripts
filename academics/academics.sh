175ce8dc600fb0b8a381f36e667cff467f942331
Kevin Zheng <coffeevector@gmail.com>
2019-04-22 15:01:43 -0700

    Simplified academics.sh and projects.sh
    Instead of opening a rofi menu and selecting the project, simply open
    ranger and have the user search for the folder instead

--- academics/academics.sh
+++ academics/academics.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-len=$(ls -l ~/Academics/Current/| wc -l)
-len=$(echo "$len -1 " | bc)
-course=$(ls -l ~/Academics/Current/ | awk '{ if (NR !=1) {print $9}}' | sed "s/\([A-Z]\)/ \1/g" | awk '{if (substr($0, 1, 1) ~ / /){print substr($0, 2)} else {print $0} }' | rofi -dmenu -lines $len -i -p "Academics")
-course=$(echo -e $course | sed "s/ //g" )
-if [ "$course" != "" ]; then
-	if [[ -f ~/Academics/Current/$course ]]; then
-		st -e rifle ~/Academics/Current/$course &
-	else
-		st -e ranger ~/Academics/Current/$course &
-	fi
-fi
+st -e ranger ~/Academics/Current/
--- projects/projects.sh
+++ projects/projects.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-len=$(ls -l ~/Projects/| wc -l)
-len=$(echo "$len -1 " | bc)
-project=$(ls -l ~/Projects | awk '{ if (NR !=1) {print $9}}' | sed "s/\([A-Z]\)/ \1/g" | awk '{if (substr($0, 1, 1) ~ / /){print substr($0, 2)} else {print $0} }' | rofi -dmenu -lines $len -i -p "Projects")
-project=$(echo -e $project | sed "s/ //g" )
-if [ "$project" != "" ]; then
-	if [[ -f ~/Projects/$project ]]; then
-		st -e rifle ~/Projects/$project &
-	else
-		st -e ranger ~/Projects/$project &
-	fi
-fi
+st -e ranger ~/Projects
