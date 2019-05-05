4cc832fe0e0c90f978ea9aae62f5987ba8472d01
Kevin Zheng <coffeevector@gmail.com>
2019-05-05 15:40:13 -0700

    Removed add/edit etc. Now just opens ranger or drive push

--- todo/todo.sh
+++ todo/todo.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
-case $(echo -e "Read\nAdd/Edit\nDelete\nDrive Push" | rofi -dmenu -i -lines 4 -p "Todo") in
-	Add/Edit)
-		file=$(ls ~/.todo | rofi -dmenu -i -p "Add/Edit")
-		if [ "$file" = "" ]; then
-			notify-send "EXITING ADD/EDIT TODO"
-			exit
-		fi
-		st -e vim ~/.todo/$file
-		;;
-	Delete)
-		file=$(ls ~/.todo | rofi -dmenu -i -p "Delete")
-		if [ "$file" = "" ]; then
-			notify-send "EXITING DELETE TODO"
-			exit
-		fi
-		rm ~/.todo/$file
-		;;
-	Read)
-		file=$(ls ~/.todo | rofi -dmenu -i -p "Read")
-		if [ "$file" = "" ]; then
-			notify-send "EXITING READ TODO"
-			exit
-		fi
-		st -e mdless ~/.todo/$file
+case $(echo -e "Open\nDrive Push" | rofi -dmenu -i -lines 2 -p "Todo") in
+	Open)
+		st -e ranger ~/.todo/
 		;;
 	Drive\ Push)
 		notifyID=$(notify-send.sh -p -t 1000000 "DRIVE PUSH IN PROGRESS")
