e83d4c7a1ebde538bbfdc638925c3b9fb5c84448
Kevin Zheng <coffeevector@gmail.com>
2019-05-05 15:59:24 -0700

    Added restic-diff | dirtree | google-chrome
    Notifies when the password is wrong, currently doesn't reprompt.
    Moves the resticDiff file to resticDiff.bak

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,13 +3,23 @@ date=$(cat ~/Backup/resticBackup/last-backup-date)
 case $(echo -e "Restic Backup\nRestic Forget\nRestic Snapshots\nRestic Prune\nDrive Push" | rofi -dmenu -i -lines 5 -p "Last Backup: $date") in
 	Restic\ Backup)
 		notifyID=$(notify-send.sh -p -t 1000000 "BACKUP IN PROGRESS")
-		output=$(rofi -dmenu -password -lines 0 -p "Password" | restic backup --exclude-file=/home/coffeevector/Backup/resticExclusions -r /home/coffeevector/Backup/resticBackup /home/coffeevector)
+		output=$(rofi -dmenu -password -lines 0 -p "Password" | restic backup --exclude-file=/home/coffeevector/Backup/resticExclusions -r /home/coffeevector/Backup/resticBackup /home/coffeevector | (head -n 1 | awk '{print $4}'; tail -n 1 | awk '{print $2}'))
 		if [ "$output" = "" ]; then
 			notify-send.sh -r $notifyID "BACKUP FAILED."
 		else
 			date +"%B %d, %Y" > ~/Backup/resticBackup/last-backup-date
 			notify-send.sh -r $notifyID "BACKUP COMPLETE."
 		fi
+		rofi -dmenu -password -lines 0 -p "Password" | restic diff -r /home/coffeevector/Backup/resticBackup $output | head -n -8 | tail -n +2 > /home/coffeevector/Backup/resticDiff
+		if [ -s "/home/coffeevector/Backup/resticDiff" ]; then
+			cat /home/coffeevector/Backup/resticDiff | dirtree -o /home/coffeevector/Backup/resticDiff.html
+			cat /home/coffeevector/Backup/resticDiff | dirtree -t treemap -o /home/coffeevector/Backup/resticDiffTreemap.html
+			google-chrome /home/coffeevector/Backup/resticDiff.html
+			google-chrome /home/coffeevector/Backup/resticDiffTreemap.html
+		else 
+			notify-send.sh -r $notifyID "RESTIC DIFF PASSWORD FAILED"
+		fi
+		mv /home/coffeevector/Backup/resticDiff /home/coffeevector/Backup/resticDiff.bak
 		;;
 	Restic\ Forget)
 		output=$(rofi -dmenu -password -lines 0 -p "Password" | restic snapshots -r /home/coffeevector/Backup/resticBackup | head -n -2 | awk '{if(NR!=1&&NR!=2){print $0}}' | tee  /tmp/restic-snapshots.txt)
