11920545d01dea76ea9f2e8cce3fd9a743497e48
Kevin Zheng <coffeevector@gmail.com>
2019-08-25 11:01:09 -0700

    Added set and unset flag for undervolt script

--- undervolt.sh
+++ undervolt.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-undervolt --core -125 --cache -125 --gpu -75
-undervolt --read
+if [ -z $1 ]; then
+    echo "Please use an argument set or unset"
+elif [ $1 = "set" ]; then
+    undervolt --core -125 --cache -125 --gpu -75
+    undervolt --read
+elif [ $1 = "unset" ]; then
+    undervolt --core 0 --cache 0 --gpu 0
+    undervolt --read
+fi
